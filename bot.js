de5ee693b5f661d98c56c83206daa992f9449c7f
ricardozangelmifiorotto <38324368+ricardozangelmifiorotto@users.noreply.github.com>
2018-04-12 11:42:21 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+
+
+bot.login('NDMwODU4NjEyMzYyMTgyNjU5.Da7RkA.NeBWaeMdWafU7e2gHXbTYSBSsXU');
+bot.on('ready', function() {
+    bot.user.setUsername("Provas");
+})
+
+bot.on('message', message => {
+    if (message.content.startsWith("!provas")) {
+        message.channel.send("3 provas marcadas.", {files: ["https://i.imgur.com/sy8oVj0.png"]})
+        
+    }  
+    });
+
+bot.on('message', message => {
+        if (message.content.startsWith("!drive")) {
+            message.channel.send("https://drive.google.com/drive/folders/0B4gCAm3CGzEFSXBUSnd6RTY2eFE")
+                
+        }  
+        });
+    
+
+bot.on('message', message => {
+            if (message.content.startsWith("!ajuda1")) {
+                message.channel.send("'!provas' para ver as provas, '!drive' para ver o link do drive")
+                    
+            }  
+            });
+        
