b3291faff579c9306c3f9c686a59389f21eb0590
ricardozangelmifiorotto <38324368+ricardozangelmifiorotto@users.noreply.github.com>
2018-05-07 14:17:14 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
 
 bot.on('message', message => {
     if (message.content.startsWith("!provas")) {
-        message.channel.send("5 provas marcadas.", {files: ["https://i.imgur.com/Sm3dFtX.png"]})
+        message.channel.send("5 provas marcadas.", {files: ["https://i.imgur.com/vijYhLM.png"]})
         
     }  
     });
