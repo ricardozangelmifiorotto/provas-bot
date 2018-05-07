7c598f13709fe5fcec9f6aa1eedeeaa475ed7924
ricardozangelmifiorotto <38324368+ricardozangelmifiorotto@users.noreply.github.com>
2018-05-07 14:09:16 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,8 +54,8 @@ bot.on('message', message => {
         });
 bot.on('message', message => {
         if (message.content.startsWith("!ps")) {
-                  message.channel.send("Essa as datas da ps.", {files: ["https://i.imgur.com/kseGiVI.png"]})
-                  message.channel.send("", {files: ["https://i.imgur.com/QWUDNx7.jpg"]})
+                  message.channel.send("Essa as datas da ps.", {files: ["https://i.imgur.com/QWUDNx7.jpg"]})
+                  message.channel.send("", {files: ["https://i.imgur.com/kseGiVI.png"]})
         }  
         });
         
